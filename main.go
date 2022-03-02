6af054852e04abfaf5ab7bec46ed9fc3be0051a6
Dif-Eq <DifEqRS@gmail.com>
2022-03-02 07:47:22 -0500

    Added some comments, so I can ever learn this crap.

--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+// All shit has this. Guessing it's like a namespace or something idk.
 package main
 
 import (
@@ -7,11 +8,15 @@ import (
 )
 
 func main() {
+	// Declares a new process and a window for it
 	a := app.New()
 	w := a.NewWindow("GoChat")
 
+	// Puts some words in the window, so you can know that it is doing anything and resizes it.
 	w.SetContent(widget.NewLabel("This better be a good platform..."))
 	w.Resize(fyne.NewSize(750, 200))
 
+	// This could be separately w.Show() and a.Run(), but you can shorthand it if you don't
+	// need to do anything else after showing whichever window.
 	w.ShowAndRun()
 }
